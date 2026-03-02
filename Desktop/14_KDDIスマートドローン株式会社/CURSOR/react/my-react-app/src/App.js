7cc3dcc85c4ff57f80820016f6fec8c9ce141877
ksdhagiwara-maker <ksd.hagiwara@gmail.com>
2026-03-02 14:29:30 +0900

    はじめての変更

---
+++ Desktop/14_KDDIスマートドローン株式会社/CURSOR/react/my-react-app/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to My React App!
+        </p>
+        <p style={{fontSize: '18px', color: '#10B981'}}>
+          I'm learning React and Git!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
